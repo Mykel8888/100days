7da0174e7656157051b3d9aae0dcbe847d79e16c
michael olayiwola <michaelolayiwola@gmail.com>
2023-11-26 12:55:02 +0100

    update the config

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRETE_KEY
+app.config['FLASK_DEBUG'] = SECRETE_KEY
 
 ckeditor = CKEditor(app)
 Bootstrap5(app)
